80ead7c86d9b88c5b695d874aa2f00e40a88ef58
Martin Fracker <martin.frackerjr@gmail.com>
2016-08-24 23:17:59 -0500

    Initialize vstudio solutions

---
+++ PGC.Dice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PGC.Dice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
